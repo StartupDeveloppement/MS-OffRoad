0200f22eca85a93dc5a36fd528dc7c522d9057e9
Hedro <tistan.vilaine@gmail.com>
2015-10-28 09:27:55 +0100

    Initial commit

---
+++ OffRoad/OffRoad/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OffRoad.Startup))]
+namespace OffRoad
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
