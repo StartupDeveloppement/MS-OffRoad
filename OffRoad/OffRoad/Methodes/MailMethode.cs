f942287bd93fae0f23d0c25e448896b795f3077c
Hedro <tistan.vilaine@gmail.com>
2016-02-10 09:54:20 +0100

    add Methode mail

---
+++ OffRoad/OffRoad/Methodes/MailMethode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+
+namespace OffRoad.Methodes
+{
+    public class MailMethode
+    {
+        public void SendMail(string to, string subject, string body) {
+            MailMessage mail = new MailMessage();
+            mail.To.Add(to);
+            mail.From = new MailAddress("Offroaddev@gmail.com");
+            mail.Subject = subject;
+            string Body = body;
+            mail.Body = Body;
+            mail.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new System.Net.NetworkCredential ("Offroaddev@gmail.com", "Offroad12345");
+            // Enter seders User name and password 
+            smtp.EnableSsl = true;
+            smtp.Send(mail); 
+        }
+    }
+}
